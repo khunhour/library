256014210192879f52d27af8b193d9633adfdcff
Khunhour Morm <khonhourmorm@gmail.com>
2021-09-18 07:12:15 +0900

    made changes to html added no books display and edit functionality

--- book.js
+++ book.js
@@ -12,28 +12,37 @@ const author = document.getElementById("author");
 const pages = document.getElementById("pages");
 const read = document.getElementsByName("readOrNot");
 const test = document.querySelector(".test");
-let readStatus;
+const submitButton = document.querySelector("#submit");
 
+
+let readStatus;
+let currentIndex;
 //when you click add new book this happen
-const submit = document.getElementById("submit");
-submit.addEventListener("click", ()=>{
+const submit = document.getElementById("format");
+submit.addEventListener("submit", submitForm);
+
+function submitForm(e){
     let processedBook = processInfo();
-    addBookToLibrary(processedBook);
+    if(e.target.children[5].children[0].className ==="edit"){
+        editBookInLibrary(processedBook);
+        submitButton.classList.remove("edit");
+        submitButton.textContent="Add Book";
+    }
+    else{
+        addBookToLibrary(processedBook);
+    }
     displayCards();
     hideForm();
-});
+    e.preventDefault();
+}
 
 //when you click cancel on form it dissapear without inserting value
 const cancel = document.getElementById("cancel");
-cancel.addEventListener("click", ()=>{
-    hideForm();
-});
+cancel.addEventListener("click", hideForm);
 
 //show pop up form when click new book, event
 const newBook = document.getElementById("newBook");
-newBook.addEventListener("click", (e) =>{
-    showForm();
-});
+newBook.addEventListener("click", showForm);
 
 //show pop up form function
 function showForm(){
@@ -50,6 +59,8 @@ function hideForm(){
 }
 //process entered info and add to object constructor
 function processInfo(){
+
+    //checking read or unread button is check on form
     for(i = 0; i< read.length; i++){
         if(read[i].checked){
             readStatus = read[i].value;
@@ -58,13 +69,23 @@ function processInfo(){
     const addBook = new book(title.value, author.value, pages.value, readStatus);
     return addBook;
 }
+
+
 //add processed book into library
 function addBookToLibrary(book){
     myLibrary.push(book);
 }
 
+
+//edit book meaning remove the old book and added the updated book into that index
+function editBookInLibrary(book){
+    myLibrary.splice(currentIndex,1, book);
+}
+
+
 //display all the objects in the array
 function displayCards(){
+    const error = document.querySelector('.noBooks');
     const content = document.querySelector(".content");
     content.textContent="";
     let i=0;           //for each id and dataset incase want to delete or edit
@@ -73,45 +94,85 @@ function displayCards(){
         const card = document.createElement('div');
         card.classList.add("card");
         
-        card.innerHTML = "<div class='info'>Title : " + object.title +"</br>"+ "Author : " + object.author + "</br>" + "Pages : "+ object.pages + "</br></div>";
-
-        if(object.read === "Read"){
-            card.innerHTML += `<div class="flexButton"><button id="${i}" class="readButton">${object.read}<img src="images/checkmark.png" alt="checkmark"></button>`+`<button data-index-number="${i}" class="readButton red">Delete</button></div>`;
-        }
-        else{
-            card.innerHTML += `<div class="flexButton"><button id="${i}" class="readButton">${object.read}</button>`+`<button data-index-number="${i}" class="readButton red">Delete</button></div>`;
-        }
+        card.innerHTML = `<div class="info">
+                                <div>Title: ${object.title}</div>
+                                <div>Author: ${object.author}</div>
+                                <div>Pages: ${object.pages}<div>
+                                <div class="flexButton" data-index=${i}>
+                                    <button id="readButton" class="readButton">${object.read === "Read"? "Read" : "Not Yet Read"}</button>
+                                    <div class="editDelete">
+                                        <button class="readButton edit">Edit</button>  
+                                        <button class="readButton red">Delete</button>
+                                    </div>
+                                </div>
+                            <div>`;
         content.appendChild(card);
         i++;
     });
+
+    //a feature to display no books when there is no books
+
+    if(content.innerHTML===""){
+        error.style.display = "block";
+    }
+    else{
+        error.style.display = "none";
+    }
     //add event listener to all read buttons
     const buttons = document.querySelectorAll('.readButton');
     buttons.forEach((button) =>{
-        button.addEventListener("click", () =>{
-            changeButton(button.id);
-        });
+        button.addEventListener("click", changeButton);
     });
     //add event listener to all delete buttons
     const deletes = document.querySelectorAll('.red');
     deletes.forEach((item) =>{
-        item.addEventListener("click", () =>{
-            deleteValue(item.dataset.indexNumber);
-            console.log(item.dataset.indexNumber);
-        })
-    })
+        item.addEventListener("click", deleteValue);
+    });
+
+    //add event listener to all edit buttons
+    const edit = document.querySelectorAll(".edit");
+    edit.forEach((item) =>{
+        item.addEventListener("click", editValue);
+    });
 }
 //change read status function
-function changeButton(buttonID){
-    if(myLibrary[buttonID].read === "Not Yet Read"){
-        myLibrary[buttonID].read = "Read";
+function changeButton(e){
+    currentIndex = e.target.parentNode.dataset.index;
+    if(myLibrary[currentIndex].read === "Not Yet Read"){
+        myLibrary[currentIndex].read = "Read";
     }
     else{
-        myLibrary[buttonID].read = "Not Yet Read";
+        myLibrary[currentIndex].read = "Not Yet Read";
     }
     displayCards();
 }
 //delete card function
-function deleteValue(index){
-    myLibrary.splice(index,1);
+function deleteValue(e){
+    currentIndex = e.target.parentNode.parentNode.dataset.index;
+    myLibrary.splice(currentIndex,1);
     displayCards();
+}
+//edit value of card info
+function editValue(e){
+    currentIndex = e.target.parentNode.parentNode.dataset.index;
+
+    title.value = myLibrary[currentIndex].title;
+    pages.value = myLibrary[currentIndex].pages;
+    author.value = myLibrary[currentIndex].author;
+
+    for(i = 0; i< read.length; i++){        //read.length is 2 inputs with radio buttons
+        if(myLibrary[currentIndex].read === read[i].value){
+            read[i].checked = true;
+        }
+    }
+            
+    const submitButton = document.querySelector("#submit");
+    submitButton.classList.add("edit");
+    submitButton.textContent="Edit";
+    showForm();
+}
+
+
+function stat(){
+
 }
